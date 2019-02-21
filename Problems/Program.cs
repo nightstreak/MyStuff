f379a4ed26bd391c3cc8bb55c78c78632733b282
nighstreak <G17M4529@ict.ru.ac.za>
2019-02-21 17:06:08 +0200

    Added the sln file, test file, and the source code.

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -6,6 +6,16 @@ namespace Problems
     {
         static void Main(string[] args)
         {
+            string Prefix(string input){
+                if(input is null){
+                    return "0,0:";
+                }
+                int wordlength = input.Length;
+                string[] splitword = new string[100];
+                splitword = input.Split(' ');
+                int wordcount = splitword.Length;
+                return $"{wordlength},{wordcount}:{input}";
+            }
             Console.WriteLine("Hello World!");
         }
     }
