1c9f59403cb33cd12775979b3accd0a5f8012629
nightstreak <prumo101@gmail.com>
2019-02-23 21:58:11 +0200

    Made one single Test

--- Problems.Test/UnitTest1.cs
+++ Problems.Test/UnitTest1.cs
@@ -20,20 +20,19 @@ namespace Tests
         {
             string results = test.Prefix("hello");
             Assert.AreEqual("5,1:hello",results);
-        }
-
-          [Test]
-        public void Test2()
-        {
             string results1 = test.Prefix("");
             Assert.AreEqual("0,0:",results1);
-        }
-        [Test]
-        public void Test3()
-        {
             string results2 = test.Prefix("what  ... did you say??");
             Assert.AreEqual( "23,6:what  ... did you say??",results2);
+            string results3 = test.Prefix("... I am ...");
+            Assert.AreEqual( "12,4:... I am ...",results3);
+            string results4 = test.Prefix("Woop... Scoop");
+            Assert.AreEqual( "13,2:Woop... Scoop",results4);
+             string results5 = test.Prefix("Hey Jump");
+            Assert.AreEqual( "8,2:Hey Jump",results5);
+            
         }
+
         
 
     }
